91782bf2ba8ac9f4c1d5859bef204bcad48ef0ee
George Halterman <george.h881@gmail.com>
2023-01-12 19:56:03 -0700

    Complete creating new todos

--- datastore/index.js
+++ datastore/index.js
@@ -8,9 +8,22 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
+      console.log('error in create');
+      callback(err);
+    }
+    var filePath = path.join(exports.dataDir, `${id}.txt`);
+    fs.writeFile(filePath, text, (err) => {
+      if (err) {
+        console.log('error in file creation');
+        callback(err);
+      } else {
+        callback(null, { id, text });
+      }
+    });
+  });
 };
 
 exports.readAll = (callback) => {
